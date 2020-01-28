cd3bef800766a994bef8e49fb09b07783365e79e
yaoyizhou1 <yauri.zhang@qunar.com>
2020-01-28 22:19:00 +0800

    init

---
+++ src/main/java/com/gaoyuan/flowsum/FlowCountReducer.java
@@ -0,0 +1,35 @@
+package com.gaoyuan.flowsum;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * 功能描述:
+ *
+ * @author yaoyizhou
+ * @date 2020/1/28 21:24
+ * @desc
+ */
+public class FlowCountReducer extends Reducer<Text, FlowBean, Text, FlowBean> {
+
+    @Override
+    protected void reduce(Text key, Iterable<FlowBean> values, Context context)throws IOException, InterruptedException {
+
+        long sum_upFlow = 0;
+        long sum_downFlow = 0;
+
+        // 1 遍历所用bean，将其中的上行流量，下行流量分别累加
+        for (FlowBean flowBean : values) {
+            sum_upFlow += flowBean.getUpFlow();
+            sum_downFlow += flowBean.getDownFlow();
+        }
+
+        // 2 封装对象
+        FlowBean resultBean = new FlowBean(sum_upFlow, sum_downFlow);
+
+        // 3 写出
+        context.write(key, resultBean);
+    }
+}
