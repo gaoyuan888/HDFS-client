3091e09dae95d7434f4324dafebc039b6d3757ca
yaoyizhou1 <yauri.zhang@qunar.com>
2020-01-29 21:46:34 +0800

    init

---
+++ src/main/java/com/gaoyuan/mr/nl/NLineReducer.java
@@ -0,0 +1,34 @@
+package com.gaoyuan.mr.nl;
+
+/**
+ * 功能描述:
+ *
+ * @author yaoyizhou
+ * @date 2020/1/29 21:42
+ * @desc
+ */
+import java.io.IOException;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class NLineReducer extends Reducer<Text, LongWritable, Text, LongWritable>{
+
+    LongWritable v = new LongWritable();
+
+    @Override
+    protected void reduce(Text key, Iterable<LongWritable> values,	Context context) throws IOException, InterruptedException {
+
+        long sum = 0L;
+
+        // 1 汇总
+        for (LongWritable value : values) {
+            sum += value.get();
+        }
+
+        v.set(sum);
+
+        // 2 输出
+        context.write(key, v);
+    }
+}
